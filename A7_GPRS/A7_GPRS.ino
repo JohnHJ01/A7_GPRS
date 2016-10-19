cf94466e84fca9ae412114199adc7173998bc842
JohnHJ01 <chanhj01@hotmail.com>
2016-10-19 10:07:51 +0800

    Initial commit

---
+++ A7_GPRS/A7_GPRS.ino
@@ -0,0 +1,12 @@
+#include <GPRS_Shield_Arduino.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
